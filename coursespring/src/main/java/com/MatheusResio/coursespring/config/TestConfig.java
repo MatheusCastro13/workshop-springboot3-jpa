077f417e5f8efc141b2e4911ce43c65567f62e4e
MatheusCastro13 <matheuscastro1304@gmail.com>
2024-01-17 11:50:51 -0300

    Category

--- coursespring/src/main/java/com/MatheusResio/coursespring/config/TestConfig.java
+++ coursespring/src/main/java/com/MatheusResio/coursespring/config/TestConfig.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.MatheusResio.coursespring.entities.Category;
 import com.MatheusResio.coursespring.entities.Order;
 import com.MatheusResio.coursespring.entities.User;
 import com.MatheusResio.coursespring.entities.enums.OrderStatus;
+import com.MatheusResio.coursespring.repositories.CategoryRepository;
 import com.MatheusResio.coursespring.repositories.OrderRepository;
 import com.MatheusResio.coursespring.repositories.UserRepository;
 
@@ -20,6 +22,9 @@ import com.MatheusResio.coursespring.repositories.UserRepository;
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -30,8 +35,14 @@ import com.MatheusResio.coursespring.repositories.UserRepository;
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 	}
 }
---
+++ coursespring/src/main/java/com/MatheusResio/coursespring/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.MatheusResio.coursespring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.MatheusResio.coursespring.entities.Category;
+import com.MatheusResio.coursespring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Integer id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
