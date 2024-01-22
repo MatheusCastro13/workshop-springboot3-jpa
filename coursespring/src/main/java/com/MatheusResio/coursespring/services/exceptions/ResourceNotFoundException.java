1497cd3e94110a689ac953f4e227638ead225f7c
MatheusCastro13 <matheuscastro1304@gmail.com>
2024-01-22 11:19:59 -0300

    Exception handling - findById

---
+++ coursespring/src/main/java/com/MatheusResio/coursespring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.MatheusResio.coursespring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
